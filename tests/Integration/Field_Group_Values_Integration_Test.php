df0c7768d53cb22df3f022e957457ee19381a64f
Tim Jensen <tim@timjensen.us>
2018-03-10 19:41:13 -0500

    Integration tests

--- tests/Integration/Field_Group_Values_Integration_Test.php
+++ tests/Integration/Field_Group_Values_Integration_Test.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Class Field_Group_Values_Test
+ * Class Field_Group_Values_Integration_Test
  *
  * @package     TimJensen\ACF\Tests
  * @author      Tim Jensen <tim@timjensen.us>
  * @license     GNU General Public License 2.0+
  * @link        https://www.timjensen.us
- * @since       2.1.2
+ * @since       2.3.0
  */
 
 namespace TimJensen\ACF\Tests;
@@ -14,11 +14,11 @@ namespace TimJensen\ACF\Tests;
 use TimJensen\ACF\Field_Group_Values;
 
 /**
- * Class Field_Group_Values_Test
+ * Class Field_Group_Values_Integration_Test
  *
  * @package TimJensen\ACF\Tests
  */
-class Field_Group_Values_Test extends TestCase {
+class Field_Group_Values_Integration_Test extends TestCase {
 
 	/**
 	 * Holds an instance of Field_Group_Values.
@@ -86,7 +86,7 @@ class Field_Group_Values_Test extends TestCase {
 			'clone',
 		];
 
-		array_walk( $expected_keys, function( $expected_key ) {
+		array_walk( $expected_keys, function ( $expected_key ) {
 			$this->assertArrayHasKey( $expected_key, $this->instance->results );
 		} );
 	}
@@ -95,21 +95,38 @@ class Field_Group_Values_Test extends TestCase {
 	 * Tests get_flexible_content_field_values().
 	 */
 	public function test_get_flexible_content_field_values() {
+		$flexcontent_field_name = $this->flexcontent_field['name'];
 
+		$this->assertArrayNotHasKey( $flexcontent_field_name, $this->instance->results );
+
+		$test_data = \get_test_data( 'post_meta' );
+
+		$this->get_protected_method_result( [
+			$this->flexcontent_field,
+			'',
+			$test_data[ $flexcontent_field_name ],
+		] );
+
+		$this->assertArrayHasKey( $flexcontent_field_name, $this->instance->results );
 	}
 
 	/**
 	 * Tests get_clone_field_values().
 	 */
 	public function test_get_clone_field_values() {
+		$clone_field_name = $this->clone_field['name'];
 
-	}
+		$this->assertArrayNotHasKey( $clone_field_name, $this->instance->results );
 
-	/**
-	 * Tests get_clone_field_config().
-	 */
-	public function test_get_clone_field_config() {
+		$test_data = \get_test_data( 'post_meta' );
 
+		$this->get_protected_method_result( [
+			$this->clone_field,
+			'',
+			$test_data[ $clone_field_name ],
+		] );
+
+		$this->assertArrayHasKey( $clone_field_name, $this->instance->results );
 	}
 
 	/**
@@ -131,21 +148,37 @@ class Field_Group_Values_Test extends TestCase {
 	 * Tests get_repeater_field_values().
 	 */
 	public function test_get_repeater_field_values() {
-		$this->assertArrayNotHasKey( $this->repeater_field['name'], $this->instance->results );
+		$repeater_field_name = $this->repeater_field['name'];
+
+		$this->assertArrayNotHasKey( $repeater_field_name, $this->instance->results );
+
+		$test_data = \get_test_data( 'post_meta' );
 
 		$this->get_protected_method_result( [
 			$this->repeater_field,
 			'',
-			1,
+			$test_data[ $repeater_field_name ],
 		] );
 
-		$this->assertArrayHasKey( $this->repeater_field['name'], $this->instance->results );
+		$this->assertArrayHasKey( $repeater_field_name, $this->instance->results );
 	}
 
 	/**
 	 * Tests get_results().
 	 */
 	public function test_get_results() {
+		$this->assertEmpty( $this->instance->results );
 
+		$results = $this->instance->get_results();
+
+		$this->assertSame(
+			$results,
+			$this->instance->results
+		);
+
+		$this->assertSame(
+			$this->instance->get_all_field_group_values( $this->instance->config ),
+			$this->instance->results
+		);
 	}
 }
